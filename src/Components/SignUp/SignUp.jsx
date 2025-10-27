6831d7978723a27b6f6f2895d8568256b1cc0b1e
ragulkannadasan <ragulkannadasan@gmail.com>
2025-10-27 14:32:41 +0000

    3

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -282,7 +282,7 @@ const SignUp = () => {
           </button>
 
           <p className="SignIn-text">
-            Already have an account   ?{" "}
+            Already have an account??{" "}
             <button
               type="button"
               className="link-button"
