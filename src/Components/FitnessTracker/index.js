91c9924a46c28a46c1bb3055a4abb00d0d127716
Ragul Kannadasan <ragulkavai@gmail.com>
2026-06-27 22:32:27 +0530

    clean

--- src/Components/FitnessTracker/index.js
+++
@@ -1,5 +0,0 @@
-export { default as FitnessTracker } from './FitnessTracker';
-export { default as Profile } from './Profile';
-export { default as MealTracker } from './MealTracker';
-export { default as WorkoutTracker } from './WorkoutTracker';
-export { default as DashboardView } from './DashboardView';
